0ca101c891dfa2a0fbe87479217a9f1a5c4e96a7
Lepoldo Caballero <git@pitakill.net>
2019-01-09 20:05:37 -0600

    Add reactstrap & Better UI

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React from 'react';
 import { provider } from './firebase';
 import firebase from 'firebase/app';
 
+import { Card, CardImg, CardBody, CardTitle, CardSubtitle, Button } from 'reactstrap';
+
 const initialState = {
-  name: 'Jone Doe',
+  name: 'John Doe',
   email: 'foo@bar.com',
   avatar: '',
   buttonMessage: 'Sign in',
@@ -13,41 +15,61 @@ const initialState = {
 class App extends React.Component {
   state = initialState
 
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(this.setUser)
+  }
+
+  setUser = (user) => {
+    if (user) {
+      this.setState({
+        name: user.displayName,
+        email: user.email,
+        avatar: user.photoURL,
+        buttonMessage: 'Sign out',
+        loggedIn: true
+      });
+
+      return;
+    }
+
+    this.setState(initialState);
+  }
+
   signIn = () => {
     firebase
       .auth()
       .signInWithPopup(provider)
-      .then(response => {
-        this.setState({
-          name: response.user.displayName,
-          email: response.user.email,
-          avatar: response.user.photoURL,
-          buttonMessage: 'Sign out',
-          loggedIn: true
-        })
-      })
       .catch(console.error)
   }
 
   signOut = () => {
-    firebase
-      .auth()
-      .signOut()
-      .then(() => {
-        this.setState(initialState)
-      })
-      .catch(console.error)
+    firebase.auth().signOut()
+  }
+
+  renderCard = () => {
+    const { avatar, email, name, buttonMessage } = this.state;
+
+    return (
+      <Card style={{width: '200px'}}>
+        <CardImg top width="100%" src={avatar} alt={name} />
+        <CardBody>
+          <CardTitle>{name}</CardTitle>
+          <CardSubtitle>{email}</CardSubtitle>
+          <br />
+          <Button onClick={this.signOut}>{buttonMessage}</Button>
+        </CardBody>
+      </Card>
+    )
   }
 
   render() {
     return (
       <>
-        { this.state.loggedIn ? <p>Bienvenido:</p> : null }
-        <div>{this.state.name}</div>
-        <div>{this.state.email}</div>
-        <img src={this.state.avatar} alt={this.state.name}/>
-        <hr />
-        <button onClick={this.state.loggedIn ? this.signOut : this.signIn }>{this.state.buttonMessage}</button>
+        {
+          this.state.loggedIn
+            ? this.renderCard()
+            : <Button onClick={this.signIn}>{this.state.buttonMessage}</Button>
+        }
       </>
     );
   }
